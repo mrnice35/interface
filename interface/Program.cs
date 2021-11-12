add3ae042ec4c4f5a2e5df7731647489c01f2822
gavrilenkovi@bk.ru <gavrilenkovi@bk.ru>
2021-11-12 18:01:43 +0300

    Добавьте файлы проекта.

---
+++ interface/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    interface Imovable //сздаем интернфейс для движущегося объекта
+    {
+        void Move();// создаем метод для движения(метод не имеет реализации)
+
+        void Brake();// метод для торможения
+    }
+    class Mercedes : Imovable //создаем класс который реализует интерфейс
+    {
+        public void Brake()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
